3ab9d43b99785efa1862dd4fa54699fafc18f953
Tom Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2019-08-02 15:44:52 +0100

    Add logo and basic dinner deciding

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val foodChoices = arrayListOf("Pizza", "Chinese", "Burgers")
+
+        selectFoodButton.setOnClickListener {
+            val randomFoodIndex = Random().nextInt(foodChoices.count())
+            selectedFoodText.text = foodChoices[randomFoodIndex]
+        }
     }
 }
